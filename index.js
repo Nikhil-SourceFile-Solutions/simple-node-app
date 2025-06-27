f23dbbc67592b492a94627a3ba2079aaa3334438
Nikhil Sasikumar <nikhil@sourcefilesolutions.com>
2025-06-27 12:37:40 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from local Node.js app!');
+    res.send('Hello from local Node.js app updated!');
 });
 
 app.listen(PORT, () => {
